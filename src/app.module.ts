da3202d0d5b7ab1571bc2e7a4c0d0750c262cf25
AlexeyZ <azv99@yandex.ru>
2024-08-13 17:24:33 +0300

    изменил докерфайл

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './controllers/users/users.module';
@@ -8,9 +9,26 @@ import { CartsModule } from './controllers/carts/carts.module';
 import { ActionsModule } from './controllers/actions/actions.module';
 
 
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/cake-app'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/cake-app'),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.DATABASE_URI, {
+      dbName: process.env.DATABASE_NAME,
+      auth: {
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+      },
+    }),
+    // MongooseModule.forRoot('mongodb://localhost:27017/', {
+    //   dbName:"cake-app",
+    //   auth: {
+    //     username: 'rootcakeapp',
+    //     password: 'cakeapppassword',
+    //   },
+    // }),
+
     UsersModule, 
     ProductsModule,
     CartsModule,
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
+const PORT = parseInt(process.env.PORT, 10) || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'), {prefix:'/public'});
@@ -13,6 +15,12 @@ async function bootstrap() {
     allowedHeaders: '*',
     credentials: true
   })
-  await app.listen(3000);
+
+  await app.listen(PORT, () => {
+    console.log(`🚀 Application running at port ${PORT}`)
+  })
+
+  // await app.listen(3000);
+  // console.log('Server START!');
 }
 bootstrap();
